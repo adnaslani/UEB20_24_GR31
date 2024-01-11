b095681203558fe95260ba94a4b5eb0ecd201c46
nanaila <naila.radoncic@student.uni-pr.edu>
2024-01-11 23:58:46 +0100

    updated

--- style/wishlist.js
+++ style/wishlist.js
@@ -1,17 +1,21 @@
 <script>
   $(document).ready(function() {
     // Handle click on the heart icon
-    $('.heart-icon').on('click', function() {
-      // Get product information
-      var productName = $(this).siblings('h3').text();
+    $('.fas.fa-heart').on('click', function() {
+      // Find the parent box of the clicked heart icon
+      var productBox = $(this).closest('.box');
 
-      // Check if the product is already in the wishlist
+      // Get product information from the box
+      var productId = productBox.data('product-id');
+      var productName = productBox.find('.content h3').text();
+
+      // Check if the product is already in the favorites
       if ($('#wishlist:contains("' + productName + '")').length === 0) {
-        // If not, add it to the wishlist
-        $('#wishlist').append('<div>' + productName + '</div>');
+        // If not, add it to the favorites
+        $('#wishlist').append('<div data-product-id="' + productId + '">' + productName + '</div>');
       } else {
-        // If it's already in the wishlist, you can provide some feedback or handle it accordingly
-        alert('Product already in wishlist!');
+        // If it's already in the favorites, you can provide some feedback or handle it accordingly
+        alert('Product already in favorites!');
       }
     });
   });
